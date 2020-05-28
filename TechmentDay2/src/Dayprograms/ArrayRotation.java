8af28f057a93cadb26eee2ce562d9db13bd580d2
Shubhy9399 <50271931+Shubhy9399@users.noreply.github.com>
2020-05-28 16:25:01 +0530

    Update ArrayRotation.java

--- TechmentDay2/src/Dayprograms/ArrayRotation.java
+++ TechmentDay2/src/Dayprograms/ArrayRotation.java
@@ -1,35 +1,37 @@
 package Dayprograms;
 
-public class ArrayRotation { 
+public class ArrayRotation {
+	public static void main(String args[]) {
     
-    void leftRotate(int arr[], int d, int n) 
-    { 
-        for (int i = 0; i < d; i++) 
-            leftRotatebyOne(arr, n); 
-    } 
+   int array[]= {1,2,3,4,5};
+   
+   int n=3;
+   
+   System.out.println("Original Array:");
   
-    void leftRotatebyOne(int arr[], int n) 
-    { 
-        int i, temp; 
-        temp = arr[0]; 
-        for (i = 0; i < n - 1; i++) 
-            arr[i] = arr[i + 1]; 
-        arr[i] = temp; 
-    } 
-  
-    
-    void printArray(int arr[], int n) 
-    { 
-        for (int i = 0; i < n; i++) 
-            System.out.print(arr[i] + " "); 
-    } 
-  
-    
-    public static void main(String[] args) 
-    { 
-        ArrayRotation rotate = new ArrayRotation(); 
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
-        rotate.leftRotate(arr, 2, 7); 
-        rotate.printArray(arr, 7); 
-    } 
-} 
+   
+   for(int i=0; i<array.length; i++)
+   {
+	   System.out.print(array[i]+ " ");
+   }
+   
+   for(int i=0;i<n;i++)
+   {
+	   int j,last;
+	   last=array[array.length-1];
+	   
+	   for (j=array.length-1;j>0;j--) {
+		   array[j]=array[j-1];
+	   }
+	   array[0]=last;
+   }
+   System.out.println();
+   
+   System.out.println("Array after rotate");
+   for(int i=0;i<array.length;i++)
+   {
+	   System.out.print(array[i]+" ");
+   }
+   
+ } 
+}
